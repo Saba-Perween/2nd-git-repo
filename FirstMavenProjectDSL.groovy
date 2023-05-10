e34593c462b4ebfc331ab0cad4734a3c6a90eaf1
Saba-Perween <128525627+Saba-Perween@users.noreply.github.com>
2023-05-10 20:26:09 +0530

    Update FirstMavenProjectDSL.groovy

--- FirstMavenProjectDSL.groovy
+++ FirstMavenProjectDSL.groovy
@@ -3,7 +3,7 @@ job('First_Maven_Project_DSL')
 	description("First maven project via DSL")
 	scm
 	{
-		git('https://github.com/Saba-Perween/2nd-git-repo.git', 'main')
+		git('https://github.com/Saba-Perween/2nd-git-repo.git', 'dev')
 	}
 	triggers
 	{
