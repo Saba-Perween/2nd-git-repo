6b4deab3edf475fd46c52303771425f6553fc546
Saba Perween <sabakhanjmi0@gmail.com>
2023-05-10 15:30:22 +0530

    changed

--- webprojectapi/src/main/java/com/test/webprojectapi/ServletInitializer.java
+++ webprojectapi/src/main/java/com/test/webprojectapi/ServletInitializer.java
@@ -3,10 +3,6 @@ package com.test.webprojectapi;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
-public class SpringBootTomcatApplication extends SpringBootServletInitializer {
-}
-
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
