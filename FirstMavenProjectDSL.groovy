0078b7b5eceb9e3a2a9daa7f3cf09944f121a1fe
Saba Perween <sabakhanjmi0@gmail.com>
2023-05-10 20:42:38 +0530

    adding

--- FirstMavenProjectDSL.groovy
+++ FirstMavenProjectDSL.groovy
@@ -3,7 +3,7 @@ job('First_Maven_Project_DSL')
 	description("First maven project via DSL")
 	scm
 	{
-		github('https://github.com/Saba-Perween/2nd-git-repo.git', 'dev')
+		git('https://github.com/Saba-Perween/2nd-git-repo.git', 'dev')
 	}
 	triggers
 	{
